49d46b3cb193e49fb47e6aa370f79ca7446dfcde
JGartner223 <85460961+JGartner223@users.noreply.github.com>
2021-11-22 21:46:17 -0700

    New Changes
    Changes

---
+++ script.js
@@ -0,0 +1,9 @@
+// Wait for document to load
+$(function() {
+    
+    // Wait for button click
+    $("#button").click(function(){
+        $("#output").css("color", "red");
+      });
+      
+})
